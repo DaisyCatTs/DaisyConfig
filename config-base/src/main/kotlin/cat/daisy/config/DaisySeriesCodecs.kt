015f92f8eae625583eee61e08a4e64ad83b7ddcc
Daisy <daisy@daisy.cat>
2026-03-29 20:27:21 +0100

    Add DaisySeries codecs for new parser families

--- config-base/src/main/kotlin/cat/daisy/config/DaisySeriesCodecs.kt
+++ config-base/src/main/kotlin/cat/daisy/config/DaisySeriesCodecs.kt
@@ -1,19 +1,39 @@
 package cat.daisy.config
 
+import cat.daisy.series.biome.DaisyBiomes
 import cat.daisy.series.enchantment.DaisyEnchantments
+import cat.daisy.series.entity.DaisyEntities
+import cat.daisy.series.gamemode.DaisyGameModes
 import cat.daisy.series.itemflag.DaisyItemFlags
 import cat.daisy.series.material.DaisyMaterials
+import cat.daisy.series.particle.DaisyParticles
 import cat.daisy.series.potion.DaisyPotions
 import cat.daisy.series.sound.DaisySounds
+import cat.daisy.series.statistic.DaisyStatistics
+import org.bukkit.GameMode
 import org.bukkit.Material
+import org.bukkit.Particle
 import org.bukkit.Sound
+import org.bukkit.Statistic
+import org.bukkit.block.Biome
 import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.EntityType
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.potion.PotionEffectType
 
 public fun materialCodec(): DaisyConfigCodec<Material> = parserBackedCodec("material", DaisyMaterials::parseOrNull)
 
 public fun soundCodec(): DaisyConfigCodec<Sound> = parserBackedCodec("sound", DaisySounds::parseOrNull)
+
+public fun biomeCodec(): DaisyConfigCodec<Biome> = parserBackedCodec("biome", DaisyBiomes::parseOrNull)
+
+public fun entityTypeCodec(): DaisyConfigCodec<EntityType> = parserBackedCodec("entity type", DaisyEntities::parseOrNull)
+
+public fun gameModeCodec(): DaisyConfigCodec<GameMode> = parserBackedCodec("game mode", DaisyGameModes::parseOrNull)
+
+public fun particleCodec(): DaisyConfigCodec<Particle> = parserBackedCodec("particle", DaisyParticles::parseOrNull)
+
+public fun statisticCodec(): DaisyConfigCodec<Statistic> = parserBackedCodec("statistic", DaisyStatistics::parseOrNull)
 
 public fun itemFlagCodec(): DaisyConfigCodec<ItemFlag> = parserBackedCodec("item flag", DaisyItemFlags::parseOrNull)
 
@@ -35,4 +55,3 @@ private fun <T> parserBackedCodec(
                 ?: DaisyDecodeResult.Failure(listOf(DaisyConfigError(path, "Unknown $type '$text'.")))
         }
     }
-
